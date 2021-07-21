edb2ada3d13948420a3d9d29e45f6ddf7684453a
Nimesh Prajapati <Nimesh Prajapati@DESKTOP-5OBRHKG>
2021-07-21 19:32:25 +0530

    commit

---
+++ helloworld/src/helloworld/ro8.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+//initialization of program
+public class ro8 {
+	public static void main(String[] args) {
+	
+//processing of program
+		int i=10;
+				int j=20;
+				int sum=i+j;
+//output of program 
+				System.out.println("the addition is"+sum);
+				
+	}
+
+}
---
+++ helloworld/src/mejava/test.java
@@ -0,0 +1,37 @@
+package mejava;
+
+public class test {
+public static void main(String[] args) {
+	System.out.println("8");
+  double d=123.45;
+  float f=(char)d;
+  System.out.println(f);
+  
+  int anar[]=new int[]{1,2,3};
+  System.out.println(anar[1]);
+
+//int i=1;
+//int j=3;
+
+//if(i==1 || j==2) {System.out.println("ok");
+ String s1="the ";
+//int x=100;
+
+
+//System.out.println(s1+x)
+{int i;
+int j;
+outer:
+
+
+
+
+;
+
+
+
+
+
+
+}
+}
